04bdfabb3d660f80fe205cb7b8ef328d78f2d493
Brandard Emuria <brandardemuria47@gmail.com>
2020-10-07 12:37:01 +0100

    First Commit
    3D Basic Game In Unity

---
+++ 3D Brand Game/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //this is a reference to the rigidbody component called ""rb
+
+    public Rigidbody rb;
+
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+
+    // We marked this as "Fixed"Update because we
+    //are using it to mess with physics
+    void FixedUpdate()
+    {
+        // Add a Forward Force
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("e")) // If the player is pressing  the "e" key
+        {
+            // Add a force to the right
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("i")) // If the player is pressing the "r" key
+        {
+            // Add a force to the left
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (rb.position.y < -1f)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
